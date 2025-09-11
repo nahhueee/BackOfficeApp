c0d38ac55349c9bd97579a150419e0bf98a131e6
nahhueee <nahu852na@gmail.com>
2025-09-11 00:41:00 -0300

    Final de pantalla clientes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navegacion } from './components/navegacion/navegacion';
 import { Toast } from 'primeng/toast';
-import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navegacion, Toast, ConfirmPopupModule],
+  imports: [RouterOutlet, Navegacion, Toast],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
